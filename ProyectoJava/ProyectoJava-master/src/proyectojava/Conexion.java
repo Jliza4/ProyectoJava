e8dfb7ffbc1762995f0b9e550fdfa70825556d07
Norman Martinez <69283053+njmartinez1@users.noreply.github.com>
2020-08-06 13:03:32 -0500

    Add files via upload

---
+++ ProyectoJava/ProyectoJava-master/src/proyectojava/Conexion.java
@@ -0,0 +1,39 @@
+package proyectojava;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Norman
+ */
+public class Conexion {
+  Connection con=null;
+  
+  public Conexion() {
+      try {
+          Class.forName("com.mysql.jdbc.Driver");
+          con=(Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/proyecto1reserva","root","12345");
+      } catch (ClassNotFoundException | SQLException e){
+          System.err.println("Error: " +e);
+      }
+  }
+    public static void main(String[] args) {
+       Conexion cn=new Conexion();
+        Statement st;
+       ResultSet rs;
+        try {
+            st=(Statement) cn.con.createStatement();
+            rs=st.executeQuery("SELECT * FROM cliente");
+            while (rs.next()){
+                System.out.println(rs.getString("cedula_cliente")+" "+rs.getString("nombre_cliente")+" "+rs.getString("telefono_cliente")+" "+rs.getString("direccion_cliente"));
+               }
+            cn.con.close();
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
+    }
+}
